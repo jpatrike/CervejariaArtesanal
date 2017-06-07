c965fa970aca91c9adf163cd59ff4b1d89f5e3b9
Rodrigo Losik <rodrigolosik@gmail.com>
2017-06-07 09:39:32 -0300

    Telas criadas, com seus respectivos nomes.

---
+++ app/src/main/java/feucbsi5/cervejariaartesanal/ListaProdutos.java
@@ -0,0 +1,13 @@
+package feucbsi5.cervejariaartesanal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaProdutos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_produtos);
+    }
+}
---
+++ app/src/main/java/feucbsi5/cervejariaartesanal/SpashScreen.java
@@ -0,0 +1,13 @@
+package feucbsi5.cervejariaartesanal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash_screen);
+    }
+}
